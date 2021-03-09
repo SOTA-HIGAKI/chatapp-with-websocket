305ef6e15283c7ed4ccc099c7c9a123848f276b1
sota0829 <souta0829win7@yahoo.co.jp>
2021-03-10 08:06:26 +0900

    aa

--- intern/settings.py
+++ intern/settings.py
@@ -130,6 +130,6 @@ LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL = '/friends'
 LOGOUT_REDIRECT_URL='/'
 try:
-    from local_settings import *
+    from .local_settings import *
 except ImportError:
     pass
